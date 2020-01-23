9ae5c60dbd42a6eb6cbabe4b3218bd187c77267a
Chris Williams <christophw+github@protonmail.com>
2020-01-23 14:54:18 +0000

    global styles

---
+++ theme/src/components/global.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Global as EmotionGlobal } from "@emotion/core";
+
+export default function Global() {
+  return (
+    <EmotionGlobal
+      styles={theme => ({
+        "*": {
+          boxSizing: `border-box`
+        },
+        "html, #___gatsby, #gatsby-focus-wrapper": {
+          margin: 0,
+          padding: 0,
+          height: "100%"
+        },
+        html: {
+          borderTop: `2px solid ${theme.colors.text}`
+        },
+        body: {
+          margin: 0,
+          padding: 0,
+          height: "100%",
+          fontSize: 14
+        }
+      })}
+    />
+  );
+}
