286315a7c47e88275d102c44c9e811baeb8dcbfd
Chris Williams <christophw+github@protonmail.com>
2020-01-30 19:12:54 +0000

    about page

---
+++ demo/src/components/about.js
@@ -0,0 +1,52 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import Img from "gatsby-image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import SEO from "gatsby-theme-cactus/src/components/seo";
+import Layout from "gatsby-theme-cactus/src/components/layout";
+
+export default function About({ data }) {
+  return (
+    <Layout>
+      <SEO title="About" description="Here's a little bit about myself" />
+      <h2 sx={{ mt: 0 }}>About Me</h2>
+      <p>
+        Hi, I’m a theme for a React framework called{" "}
+        <a
+          href="https://www.gatsbyjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gatsby
+        </a>
+        . I’m particularly great for a personal web page with a simple blog.
+      </p>
+      <Img
+        fluid={data.aboutImg.childImageSharp.fluid}
+        alt="Picture of a cactus"
+        sx={{ mx: `auto`, my: [4], maxWidth: 200 }}
+      />
+      <p>These are my best features</p>
+      <ul>
+        <li>I am fully responsive</li>
+        <li>I support Google Analytics</li>
+        <li>I have a configurable navigation menu</li>
+        <li>I have Font Awesome icons</li>
+        <li>I'm truly the most tsundere of plants</li>
+      </ul>
+      <p>
+        Clone or fork{" "}
+        <a
+          href="https://github.com/chrismwilliams/gatsby-theme-cactus"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          my <FontAwesomeIcon icon={faGithub} /> repo
+        </a>{" "}
+        if you like me!
+      </p>
+    </Layout>
+  );
+}
