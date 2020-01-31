65f62d2369370e4b26dd60bb403cb52251acb1d5
Chris Williams <christophw+github@protonmail.com>
2020-01-31 14:11:27 +0000

    change to merge; styling blog header

--- demo/src/gatsby-plugin-theme-ui/index.js
+++ demo/src/gatsby-plugin-theme-ui/index.js
@@ -1,11 +1,12 @@
 // Example of overriding & merging base (gatsby-theme-cactus) styles with your own
-import merge from "lodash.merge";
-import cactusTheme from "gatsby-theme-cactus/src/gatsby-plugin-theme-ui";
+import baseTheme from "gatsby-theme-cactus/src/gatsby-plugin-theme-ui";
 import dracula from "@theme-ui/prism/presets/dracula.json";
 
-// lodash.merge will deeply merge custom values with the blog theme's defaults
-export default merge({}, cactusTheme, {
+// merge custom values with the blog theme's defaults
+export default {
+  ...baseTheme,
   styles: {
+    ...baseTheme.styles,
     code: {
       ...dracula
     },
@@ -22,6 +23,96 @@ export default merge({}, cactusTheme, {
     }
   },
   layout: {
+    ...baseTheme.layout,
+    blogHeader: {
+      position: `fixed`,
+      top: `2rem`,
+      right: 0,
+      pr: [2, 5],
+      display: `grid`,
+      gridTemplateColumns: `repeat(2,1fr)`,
+      gridTemplateRows: `repeat(2,1fr)`,
+      alignItems: `flex-start`,
+      zIndex: 999,
+      nav: {
+        gridColumn: `1 / -1`,
+        gridRow: `1 / -1`,
+        display: `flex`,
+        flexDirection: `column`,
+        ".page-links": {
+          pt: `0.25rem`,
+          pr: 3,
+          lineHeight: `15px`,
+          letterSpacing: `0.01em`,
+          fontSize: `0.8rem`,
+          fontWeight: `200`,
+          fontStyle: `normal`,
+          span: {
+            display: `inline-block`,
+            marginRight: [3],
+            borderRight: `1px dotted`,
+            borderRightColor: `primary`,
+            ":last-of-type": {
+              borderRight: 0
+            }
+          },
+          a: {
+            marginRight: [3],
+            color: `primary`,
+            textDecoration: `none`
+          }
+        },
+        ".blog-links": {
+          mt: 4,
+          display: [`none`, `initial`],
+          alignSelf: `flex-end`,
+          a: {
+            mx: `0.75rem`,
+            color: `accent`,
+            ":hover": {
+              color: `secondary`
+            }
+          },
+          "[data-tooltip]": {
+            position: `relative`,
+            "::after": {
+              opacity: 0,
+              visibility: `hidden`,
+              position: `absolute`,
+              content: `attr(data-tooltip)`,
+              top: `1.5em`,
+              left: `50%`,
+              transform: `translateX(-50%) translateY(-2px)`,
+              fontStyle: `italic`,
+              color: `text`,
+              whiteSpace: `nowrap`,
+              zIndex: 999,
+              transition: `opacity 0.2s cubic-bezier(0.64, 0.09, 0.08, 1)`
+            },
+            ":hover": {
+              "::after": {
+                display: `block`,
+                opacity: 1,
+                visibility: `visible`,
+                transform: `translateX(-50%) translateY(0)`
+              }
+            }
+          }
+        }
+      },
+      ".bg-menu": {
+        gridColumn: 2,
+        gridRow: 1,
+        justifySelf: `end`,
+        background: `none`,
+        outline: `none`,
+        color: `accent`,
+        border: 0,
+        ":hover": {
+          cursor: `pointer`
+        }
+      }
+    },
     post: {
       ".meta": {
         display: `flex`,
@@ -91,4 +182,4 @@ export default merge({}, cactusTheme, {
       }
     }
   }
-});
+};
