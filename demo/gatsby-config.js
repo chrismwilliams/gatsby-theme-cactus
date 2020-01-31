2a19bf42e8a1dc63980e859fb3d6b40eb596fae2
Chris Williams <christophw+github@protonmail.com>
2020-01-31 09:05:58 +0000

    move site plugins to demo

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,12 +1,36 @@
 /* 
-  TODO
+  ** Your probably going to want to override a few settings in here, such as the siteMetadata and the manifest settings ** 
+  Any options added to gatsby-theme-cactus will be directly passed to gatsby-theme-blog-core
 */
 
 module.exports = {
+  siteMetadata: {
+    lang: `en-GB`
+  },
   plugins: [
     {
       resolve: "gatsby-theme-cactus",
       options: {}
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID // create an env file with this key and your analytics id
+      }
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-theme-cactus`,
+        short_name: `gatsby-theme-cactus`,
+        start_url: `/`,
+        background_color: `#1d1f21`,
+        theme_color: `#2bbc8a`,
+        display: `standalone`,
+        icon: `content/assets/logo.png` // This path is relative to the root of the site.
+      }
+    },
+    `gatsby-plugin-offline`
   ]
 };
--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -13,32 +13,12 @@ module.exports = options => {
       social: [{ name: `GitHub`, url: `https://github.com/chrismwilliams` }]
     },
     plugins: [
-      `gatsby-plugin-react-helmet`,
-      `gatsby-plugin-sitemap`,
-      `gatsby-plugin-theme-ui`,
       {
         resolve: `gatsby-theme-blog-core`,
         options
       },
-      {
-        resolve: `gatsby-plugin-google-analytics`,
-        options: {
-          trackingId: process.env.GOOGLE_ANALYTICS_ID // create an env file with this key and your analytics id
-        }
-      },
-      {
-        resolve: `gatsby-plugin-manifest`,
-        options: {
-          name: `gatsby-theme-cactus`,
-          short_name: `cactus`,
-          start_url: `/`,
-          background_color: `#1d1f21`,
-          theme_color: `#2bbc8a`,
-          display: `standalone`,
-          icon: `content/assets/logo.png` // This path is relative to the root of the site.
-        }
-      },
-      `gatsby-plugin-offline`
-    ].filter(Boolean)
+      `gatsby-plugin-react-helmet`,
+      `gatsby-plugin-theme-ui`
+    ]
   };
 };
