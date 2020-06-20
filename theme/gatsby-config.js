2748604c5d537c75147e92d058affd9f02082659
Chris Williams <me@chriswilliams.co>
2020-06-20 15:27:18 +0100

    add mdx; remove blog-core

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,4 +1,8 @@
-module.exports = options => {
+const withDefaults = require(`./utils/default-options`);
+
+module.exports = (themeOptions) => {
+  const options = withDefaults(themeOptions);
+
   return {
     siteMetadata: {
       title: `Gatsby Theme Cactus`,
@@ -8,12 +12,49 @@ module.exports = options => {
       lang: `en`,
     },
     plugins: [
-      {
-        resolve: `gatsby-theme-blog-core`,
-        options,
-      },
       `gatsby-plugin-react-helmet`,
       `gatsby-plugin-theme-ui`,
+      {
+        resolve: `gatsby-plugin-mdx`,
+        options: {
+          extensions: [`.mdx`, `.md`],
+          gatsbyRemarkPlugins: [
+            `gatsby-remark-smartypants`,
+            `gatsby-remark-responsive-iframe`,
+            `gatsby-remark-copy-linked-files`,
+            {
+              resolve: `gatsby-remark-autolink-headers`,
+              options: {
+                icon: false,
+              },
+            },
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 1280,
+                linkImagesToOriginal: false,
+                withWebp: true,
+              },
+            },
+          ],
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: options.contentPath,
+          name: options.contentPath,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: options.assetPath,
+          name: options.assetPath,
+        },
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
     ],
   };
 };
